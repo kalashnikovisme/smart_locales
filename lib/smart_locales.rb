e2a45a65c3ac56caecc6f6d4847ad7d411a0b0d5
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-10-30 02:52:17 +0400

    updates

--- lib/smart_locales.rb
+++ lib/smart_locales.rb
@@ -3,6 +3,7 @@ require "smart_locales/title"
 require "smart_locales/title/helpers"
 
 module SmartLocales
-  ActionView::Base.send :include, SmartLocales::Title::Helpers
+  gem_root = Gem::Specification.find_by_name('smart_locales').gem_dir
   I18n.load_path << gem_root + '/lib/smart_locales/title/helpers.yml'
+  ActionView::Base.send :include, SmartLocales::Title::Helpers
 end
